eaf829cb6b40d0237e4b6145abea4ae619a56a41
Joseph Zhong <joseph.zhong@hotmail.com>
2016-10-16 02:58:26 -0700

    validated json

--- learning/plot.py
+++ learning/plot.py
@@ -6,8 +6,8 @@ import json
 data_file = open('./data.json', 'r')
 for line in data_file:
   print line
-  # d = json.loads(line)
-  # print d
+  d = json.loads(line)
+  print d
 #
 # val = 0. # this is the value where you want the data to appear on the y-axis.
 # ar = np.arange(10) # just as an example array
