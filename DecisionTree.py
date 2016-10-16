21d0f9edc3ca7c52f64f1851541fa24fbfbbdaf2
rosiezou <rosiezou@gmail.com>
2016-10-16 05:24:31 -0400

    initial pseudo code for DT algo

---
+++ DecisionTree.py
@@ -0,0 +1,18 @@
+from sklearn import tree
+
+# parse data from MS Cognitive Services API
+n = [] # input needs to be a listof float for each pic
+
+X = []
+X.append(n)
+
+# for confused faces
+Y.append(1)
+
+# for non-confused faces
+Y.append(0)
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X, Y)
+
+#output
+retval = clf.predict([[test val]])
